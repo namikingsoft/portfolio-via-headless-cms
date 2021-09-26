bfb2c1fe6542bb698c6c93596ed4cf7f1493606a
Tsubasa Namiki <namikingsoft@gmail.com>
2021-09-26 17:18:03 +0900

    Set up gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const { NEXT_PUBLIC_GTAG_ID } = process.env
+    if (NEXT_PUBLIC_GTAG_ID == null) return () => {}
+
+    const handleRouteChange = (url: string) => {
+      window.gtag('config', NEXT_PUBLIC_GTAG_ID, {
+        page_path: url,
+      })
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return <Component {...pageProps} />
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,29 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
   render() {
+    const { NEXT_PUBLIC_GTAG_ID } = process.env
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {NEXT_PUBLIC_GTAG_ID && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GTAG_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${NEXT_PUBLIC_GTAG_ID}');
+                  `.replace(/\r?\n\s*/gs, ''),
+                }}
+              />
+            </>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />

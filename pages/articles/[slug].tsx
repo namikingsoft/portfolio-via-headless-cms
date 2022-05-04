1c48ff3f91e17902df597fb26d7c153a64199e99
Tsubasa Namiki <namikingsoft@gmail.com>
2022-05-05 08:49:39 +0900

    Adjust article style from post

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -2,10 +2,11 @@ import Head from 'next/head'
 import Container from '../../components/container'
 import Header from '../../components/header'
 import Layout from '../../components/layout'
+import ArticleBody from '../../components/article-body'
+import ArticleHeader from '../../components/article-header'
 import markdownToHtml from '../../lib/markdownToHtml'
 import { CMS_NAME } from '../../lib/constants'
 import { getAllArticles, getArticle, Article } from '../../lib/contentful'
-import markdownStyles from '../../components/markdown-styles.module.css'
 
 type Props = {
   article: Article
@@ -15,24 +16,21 @@ type Props = {
 const Post = ({ article, content }: Props) => {
   return (
     <Layout>
-      <Head>
-        <title>
-          {article.title} | Next.js Blog Example with {CMS_NAME}
-        </title>
-      </Head>
       <Container>
         <Header />
-        <section>
-          <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-            {article.title}
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
-            <div
-              className={markdownStyles.markdown}
-              dangerouslySetInnerHTML={{ __html: content }}
-            />
-          </div>
-        </section>
+        <article className="mb-32">
+          <Head>
+            <title>
+              {article.title} | Next.js Blog Example with {CMS_NAME}
+            </title>
+          </Head>
+          <ArticleHeader
+            title={article.title || ''}
+            coverImage={article.thumbnail?.url || ''}
+            date={article.sys.publishedAt}
+          />
+          <ArticleBody content={content} />
+        </article>
       </Container>
     </Layout>
   )

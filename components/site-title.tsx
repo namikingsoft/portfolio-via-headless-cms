0aab21db4863cb5e4f00372a4af6fe231e71bb12
Tsubasa Namiki <namikingsoft@gmail.com>
2022-05-08 16:20:55 +0900

    Change cms name

--- components/site-title.tsx
+++ components/site-title.tsx
@@ -4,7 +4,7 @@ const SiteTitle = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        {CMS_NAME}
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A statically generated blog example using{' '}
@@ -14,7 +14,7 @@ const SiteTitle = () => {
         >
           Next.js
         </a>{' '}
-        and {CMS_NAME}.
+        and Markdown.
       </h4>
     </section>
   )

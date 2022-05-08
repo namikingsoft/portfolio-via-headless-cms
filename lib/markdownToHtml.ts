0434aa61d87951beba860690c5df47e2d349c091
Tsubasa Namiki <namikingsoft@gmail.com>
2022-05-08 21:53:49 +0900

    Support plantuml on markdown

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,11 @@
 import { remark } from 'remark'
 import html from 'remark-html'
+import simplePlantUML from '@akebifiky/remark-simple-plantuml'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html, { sanitize: false }).process(markdown)
+  const result = await remark()
+    .use(html, { sanitize: false })
+    .use(simplePlantUML)
+    .process(markdown)
   return result.toString()
 }

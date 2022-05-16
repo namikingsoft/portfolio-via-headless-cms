ca357fc2ec25235a48b7211ce8bda8afb2409256
Tsubasa Namiki <namikingsoft@gmail.com>
2022-05-16 21:16:39 +0900

    Disable swc for build on vercel

---
+++ next.json.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // Fix for SWC Failed to Load
+  // https://nextjs.org/docs/messages/failed-loading-swc
+  // https://stackoverflow.com/questions/69816589/next-failed-to-load-swc-binary/69820670#69820670
+  swcMinify: false,
+}

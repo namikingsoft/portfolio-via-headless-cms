dc427c0c2de0c0ba403558e95392492706c2f8a2
Tsubasa Namiki <namikingsoft@gmail.com>
2022-05-24 06:24:37 +0900

    Return http status code 400 on throw error temporarily

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from 'fs'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { ApolloServer } from 'apollo-server-micro'
+import { PluginDefinition } from 'apollo-server-core'
 import { context } from '../../bff/context'
 import { resolvers } from '../../bff/resolvers'
 import { bffGraphQLEndpoint } from '../../lib/constants'
@@ -9,11 +10,24 @@ const schemaPath = `${process.cwd()}/bff/schema.graphql`
 
 const typeDefs = readFileSync(schemaPath, { encoding: 'utf-8' })
 
+const setHttpPlugin: PluginDefinition = {
+  async requestDidStart() {
+    return {
+      async willSendResponse({ response }) {
+        if (response.http && response?.errors) {
+          response.http.status = 400
+        }
+      },
+    }
+  },
+}
+
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
   context,
   csrfPrevention: true,
+  plugins: [setHttpPlugin],
 })
 
 const startServer = apolloServer.start()

9ff14c940f909fd9b477f91ada65cb011d83da17
Tsubasa Namiki <namikingsoft@gmail.com>
2022-05-31 04:05:18 +0900

    Implement original form validation error

--- bff/errors.ts
+++ bff/errors.ts
@@ -1,19 +1,29 @@
 import type { GraphQLError } from 'graphql'
 
-type FormValidationError = {
+export class FormValidationError extends Error {
+  constructor(
+    message: string,
+    public fieldName: string,
+    public fieldMessage: string,
+  ) {
+    super(message)
+  }
+}
+
+type FormValidationErrorObject = {
   fieldName: string
   fieldMessage: string
 }
 
-const isFormValidationError = (x: any): x is FormValidationError =>
-  'fieldName' in x && 'fieldMessage' in x
+const isFormValidationError = (x: any): x is FormValidationErrorObject =>
+  !!x && 'fieldName' in x && 'fieldMessage' in x
 
 export const parseToFormValidationErrors = (
   errors: GraphQLError[],
 ): FormValidationError[] =>
   errors
     .map((err) => {
-      const originalError = err.originalError
+      const originalError = err.originalError || err
       if (isFormValidationError(originalError)) {
         return originalError
       }
@@ -29,6 +39,8 @@ export const formatError = (error: GraphQLError) => {
 
   if (originalError?.code === 'ERR_GRAPHQL_CONSTRAINT_VALIDATION') {
     return { ...originalError, fieldMessage: originalError?.message, ...error }
+  } else if (originalError instanceof FormValidationError) {
+    return { ...originalError }
   }
   return error
 }
--- bff/resolvers/mutation/authenticate.ts
+++ bff/resolvers/mutation/authenticate.ts
@@ -2,12 +2,21 @@ import { Resolvers } from '../../server.generated'
 import { getVisitorByPassword } from '../../../contentful'
 import { fernet } from '../../fernet'
 import { visitorTokenMaxAgeSec } from '../../../lib/constants'
+import { FormValidationError } from '../../errors'
 
 export const authenticate: NonNullable<
   NonNullable<Resolvers['Mutation']>['authenticate']
 > = async (parent, args, context) => {
-  const visitor = await getVisitorByPassword(args.input.password)
-  const token = await fernet.sign(visitor.username, visitorTokenMaxAgeSec)
-  context.setCookieToken(token)
-  return { token, visitor }
+  try {
+    const visitor = await getVisitorByPassword(args.input.password)
+    const token = await fernet.sign(visitor.username, visitorTokenMaxAgeSec)
+    context.setCookieToken(token)
+    return { token, visitor }
+  } catch (err) {
+    throw new FormValidationError(
+      'Incorect password',
+      'password',
+      'Incorect password',
+    )
+  }
 }

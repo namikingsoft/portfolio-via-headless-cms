a8e5d21a26d7e59c5560c36331f2f130a3d72cbd
Tsubasa Namiki <namikingsoft@gmail.com>
2022-05-31 04:08:38 +0900

    Avoid alert component

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,19 +1,16 @@
 import React from 'react'
-import Alert from './alert'
 import Footer from './footer'
 import Meta from './meta'
 
 type Props = {
-  preview?: boolean
   children: React.ReactNode
 }
 
-const Layout = ({ preview, children }: Props) => {
+const Layout = ({ children }: Props) => {
   return (
     <>
       <Meta />
       <div className="min-h-screen">
-        <Alert preview={preview} />
         <main>{children}</main>
       </div>
       <Footer />

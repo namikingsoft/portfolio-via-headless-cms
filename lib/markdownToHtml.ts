27070abe91c79da622444108a9bc815cf544e409
Tsubasa Namiki <namikingsoft@gmail.com>
2022-06-12 01:23:16 +0900

    Add some remark plugin

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,11 +1,17 @@
 import { remark } from 'remark'
 import html from 'remark-html'
 import prism from 'remark-prism'
+import gfm from 'remark-gfm'
+import math from 'remark-math'
+import emoji from 'remark-emoji'
 import simplePlantUML from '@akebifiky/remark-simple-plantuml'
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
+    .use(gfm)
+    .use(math)
     .use(simplePlantUML)
+    .use(emoji)
     .use(prism)
     .use(html, { sanitize: false })
     .process(markdown)

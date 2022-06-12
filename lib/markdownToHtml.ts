93c20cb94790861f7f68dd90e263cf2f7be9dccc
Tsubasa Namiki <namikingsoft@gmail.com>
2022-06-13 07:20:58 +0900

    Support toc on markdown

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -2,6 +2,7 @@ import { remark } from 'remark'
 import html from 'remark-html'
 import prism from 'remark-prism'
 import gfm from 'remark-gfm'
+import toc from 'remark-toc'
 import math from 'remark-math'
 import emoji from 'remark-emoji'
 import simplePlantUML from '@akebifiky/remark-simple-plantuml'
@@ -9,6 +10,7 @@ import simplePlantUML from '@akebifiky/remark-simple-plantuml'
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
     .use(gfm)
+    .use(toc, { heading: 'toc|目次', maxDepth: 2 })
     .use(math)
     .use(simplePlantUML)
     .use(emoji)

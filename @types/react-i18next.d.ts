83cad32832b2c774611a649124b006c00a435772
Tsubasa Namiki <namikingsoft@gmail.com>
2022-06-20 04:23:47 +0900

    Add type on translation

---
+++ @types/react-i18next.d.ts
@@ -0,0 +1,11 @@
+import 'react-i18next'
+import common from '../public/locales/en/common.json'
+
+declare module 'react-i18next' {
+  // eslint-disable-next-line no-unused-vars -- overwrite
+  interface CustomTypeOptions {
+    resources: {
+      common: typeof common
+    }
+  }
+}

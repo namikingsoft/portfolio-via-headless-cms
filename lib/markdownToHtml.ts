f8c85dc52d65bfc6527f664f406957650ee379f1
Tsubasa Namiki <namikingsoft@gmail.com>
2022-07-17 00:31:02 +0900

    Support katex for render math on markdown

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -4,6 +4,7 @@ import prism from 'remark-prism'
 import gfm from 'remark-gfm'
 import toc from 'remark-toc'
 import math from 'remark-math'
+import katex from 'remark-html-katex'
 import emoji from 'remark-emoji'
 import simplePlantUML from '@akebifiky/remark-simple-plantuml'
 
@@ -12,6 +13,7 @@ export default async function markdownToHtml(markdown: string) {
     .use(gfm)
     .use(toc, { heading: 'toc|TOC|Table of Contents|目次', maxDepth: 2 })
     .use(math)
+    .use(katex)
     .use(simplePlantUML)
     .use(emoji)
     .use(prism)
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,12 @@ export default class MyDocument extends Document {
             type="text/css"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/brands.min.css"
           />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css"
+            integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
+            crossOrigin="anonymous"
+          />
           {gtagId && (
             <>
               <script

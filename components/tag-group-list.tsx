b4862b78d6775565f4e95f86542c12ba826322c4
Tsubasa Namiki <namikingsoft@gmail.com>
2022-10-16 20:58:33 +0900

    Adjust tag group list style

--- components/tag-group-list.tsx
+++ components/tag-group-list.tsx
@@ -12,12 +12,12 @@ const TagGroupList = ({ tagGroups }: Props) => {
       {tagGroups.map((tagGroup) => (
         <section key={tagGroup.title}>
           <h3 className="mt-5 mb-2 font-bold text-xl">{tagGroup.title}</h3>
-          <ul className="flex flex-row gap-5 flex-wrap">
+          <ul className="flex flex-row gap-x-5 gap-y-2 flex-wrap mb-7">
             {tagGroup.tags.map((tag) => (
               <li key={tag.slug}>
                 <Link href={pagesPath.private.tags._slug(tag.slug).$url()}>
-                  <a className="hover">
-                    {tag.title} ({tag.total})
+                  <a className="hover hover:underline">
+                    {tag.title} <span className="font-thin">({tag.total})</span>
                   </a>
                 </Link>
               </li>

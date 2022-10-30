be27ce2d8692e42c77e62858fde56852e5138c66
Tsubasa Namiki <namikingsoft@gmail.com>
2022-10-31 05:47:57 +0900

    Set fill image sizes

--- components/fill-image.tsx
+++ components/fill-image.tsx
@@ -26,6 +26,17 @@ const FillImage = ({
       alt={alt}
       width={width}
       height={height}
+      sizes="
+        (max-width: 640px) 640px,
+        (max-width: 800px) 800px,
+        (max-width: 1024px) 1024px,
+        (max-width: 1280px) 1280px,
+        (max-width: 1440px) 1440px,
+        (max-width: 1680px) 1680px,
+        (max-width: 1920px) 1920px,
+        (max-width: 2560px) 2560px,
+        3840px
+      "
       className={cn(
         'object-cover min-w-full',
         { 'aspect-video': !aspectRatio },

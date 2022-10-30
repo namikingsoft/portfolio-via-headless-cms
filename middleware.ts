21a74c03065536497644fd615727636d24021942
Tsubasa Namiki <namikingsoft@gmail.com>
2022-10-31 01:00:27 +0900

    Migrate middleware
    refs. https://nextjs.org/docs/messages/middleware-upgrade-guide

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { FernetLike } from '../../lib/fernetLike'
+import { FernetLike } from './lib/fernetLike'
 import {
   redirectUriSearchParamsName,
   visitorTokenCookieName,
-} from '../../lib/constants'
-import { pagesPath } from '../../lib/$path'
+} from './lib/constants'
+import { pagesPath } from './lib/$path'
 import {
   visitorTokenSignerKey128 as signerKey,
   visitorTokenCryptoKey128 as cryptoKey,
-} from '../../env'
+} from './env'
 
 const fernet = new FernetLike({ signerKey, cryptoKey, crypto })
 
 export async function middleware(req: NextRequest) {
   try {
-    await fernet.verify(req.cookies[visitorTokenCookieName])
+    const visitorToken = req.cookies.get(visitorTokenCookieName)
+    if (!visitorToken) throw new Error('Visitor token not found')
+    await fernet.verify(visitorToken)
     return NextResponse.next()
   } catch (err) {
     const url = req.nextUrl.clone()
@@ -22,4 +24,8 @@ export async function middleware(req: NextRequest) {
     url.pathname = pagesPath.$url().pathname
     return Response.redirect(url)
   }
+}
+
+export const config = {
+  matcher: ['/private/:path*'],
 }

a7232a79fb604fa3d1973a87d63dcd00b414957b
Tsubasa Namiki <namikingsoft@gmail.com>
2022-11-03 16:43:59 +0900

    Implement hook of useZoomImage

---
+++ lib/useZoomImage.spec.tsx
@@ -0,0 +1,62 @@
+/**
+ * @jest-environment jsdom
+ */
+
+/* eslint-disable @next/next/no-img-element -- for test */
+
+import { render, fireEvent } from '@testing-library/react'
+import { useZoomImage } from './useZoomImage'
+
+const Usage = () => {
+  const zoomImage = useZoomImage('a[href$=".png"]')
+
+  return (
+    <>
+      <a href="https://example.com/test.png" data-testid="link1">
+        <img src="https://example.com/test.png" alt="test1" />
+      </a>
+      <a href="https://example.com/test.png" data-testid="link2">
+        <img src="https://example.com/different.png" alt="test2" />
+      </a>
+      <a href="https://example.com/test.gif" data-testid="link3">
+        <img src="https://example.com/test.gif" alt="test3" />
+      </a>
+      {zoomImage}
+    </>
+  )
+}
+
+test('useZoomImage', () => {
+  const { getByTestId } = render(<Usage />)
+
+  const link1 = getByTestId('link1') as HTMLAnchorElement
+  const link2 = getByTestId('link2') as HTMLAnchorElement
+  const link3 = getByTestId('link3') as HTMLAnchorElement
+  const findZoomImage = (): HTMLImageElement =>
+    document.body.querySelector('img[alt="Zoom Image"]')!
+
+  expect(link1.className).toBe('cursor-zoom-in')
+  expect(findZoomImage()).toBeNull()
+
+  fireEvent.click(link1)
+
+  expect(findZoomImage()).not.toBeNull()
+  expect(findZoomImage().src).toBe('https://example.com/test.png')
+
+  fireEvent.click(findZoomImage())
+
+  expect(findZoomImage()).toBeNull()
+
+  fireEvent.click(link2)
+
+  expect(findZoomImage()).not.toBeNull()
+  expect(findZoomImage().src).toBe('https://example.com/different.png')
+
+  fireEvent.click(findZoomImage())
+
+  expect(findZoomImage()).toBeNull()
+
+  fireEvent.click(link3)
+
+  expect(findZoomImage()).toBeNull()
+})
---
+++ lib/useZoomImage.tsx
@@ -0,0 +1,92 @@
+import { MdClose } from 'react-icons/md'
+import { ReactNode, useEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
+
+/* eslint-disable @next/next/no-img-element -- via CMS */
+
+type Props = {
+  src: string
+  onClose: () => void
+}
+
+const ZoomImage = ({ src, onClose }: Props) => {
+  const [initilized, setInitialized] = useState(false)
+
+  useEffect(() => {
+    setInitialized(true)
+  }, [])
+
+  return (
+    globalThis?.document &&
+    createPortal(
+      <div
+        className={`fixed top-0 left-0 bottom-0 right-0 transition duration-300 bg-black ${
+          initilized ? 'bg-opacity-75' : 'bg-opacity-0'
+        }`}
+        onClick={onClose}
+      >
+        <div className="w-full h-full text-center p-0 md:p-16 overflow-scroll">
+          <img
+            src={src}
+            alt="Zoom Image"
+            className={`transition-transform duration-300 ${
+              initilized ? 'scale-100' : 'scale-0'
+            }`}
+          />
+        </div>
+        <div className="absolute top-5 right-5 rounded-full p-3 bg-black hover:bg-opacity-100 cursor-pointer bg-opacity-50 drop-shadow">
+          <MdClose className=" text-gray-300 hover:text-white cursor-pointer text-3xl drop-shadow-md" />
+        </div>
+      </div>,
+      globalThis.document.body,
+    )
+  )
+}
+
+export const useZoomImage = (selector: string): ReactNode => {
+  const [zoomImageSrc, setZoomImageSrc] = useState<string>()
+
+  useEffect(() => {
+    const handleClick = (event: Event) => {
+      event.preventDefault()
+      const eventTarget = event.currentTarget
+      if (eventTarget instanceof HTMLAnchorElement) {
+        const thumbSrc = eventTarget.querySelector('img')!.src
+        setZoomImageSrc(
+          thumbSrc === eventTarget.href ? eventTarget.href : thumbSrc,
+        )
+      }
+    }
+
+    document.querySelectorAll(selector).forEach((elm) => {
+      elm.addEventListener('click', handleClick)
+    })
+
+    return () => {
+      document.querySelectorAll(selector).forEach((elm) => {
+        elm.removeEventListener('click', handleClick)
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    document.querySelectorAll(selector).forEach((elm) => {
+      elm.classList.add('cursor-zoom-in')
+    })
+
+    return () => {
+      document.querySelectorAll(selector).forEach((elm) => {
+        elm.classList.remove('cursor-zoom-in')
+      })
+    }
+  })
+
+  return (
+    zoomImageSrc && (
+      <ZoomImage
+        src={zoomImageSrc}
+        onClose={() => setZoomImageSrc(undefined)}
+      />
+    )
+  )
+}

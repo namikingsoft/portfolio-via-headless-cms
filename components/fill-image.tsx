78677ac10f8ada6a4b6a10358a4db381ef99b0d5
Tsubasa Namiki <namikingsoft@gmail.com>
2022-11-06 14:51:19 +0900

    Fix fill image without blur src

--- components/fill-image.tsx
+++ components/fill-image.tsx
@@ -24,35 +24,41 @@ const FillImage = ({
   blurSrc,
   className,
 }: Props) => {
-  return (
+  const imageElement = (
+    <Image
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      sizes="
+  (max-width: 640px) 640px,
+  (max-width: 800px) 800px,
+  (max-width: 1024px) 1024px,
+  (max-width: 1280px) 1280px,
+  (max-width: 1440px) 1440px,
+  (max-width: 1680px) 1680px,
+  (max-width: 1920px) 1920px,
+  (max-width: 2560px) 2560px,
+  3840px
+"
+      priority={priority}
+      className={cn(
+        'object-cover min-w-full min-h-full',
+        objectPosition === 'top' ? 'object-top' : 'object-center',
+      )}
+      style={{ aspectRatio }}
+    />
+  )
+
+  return blurSrc ? (
     <div
       className={cn(className, 'bg-cover')}
       style={{ backgroundImage: `url(${blurSrc})` }}
     >
-      <Image
-        src={src}
-        alt={alt}
-        width={width}
-        height={height}
-        sizes="
-        (max-width: 640px) 640px,
-        (max-width: 800px) 800px,
-        (max-width: 1024px) 1024px,
-        (max-width: 1280px) 1280px,
-        (max-width: 1440px) 1440px,
-        (max-width: 1680px) 1680px,
-        (max-width: 1920px) 1920px,
-        (max-width: 2560px) 2560px,
-        3840px
-      "
-        priority={priority}
-        className={cn(
-          'object-cover min-w-full min-h-full',
-          objectPosition === 'top' ? 'object-top' : 'object-center',
-        )}
-        style={{ aspectRatio }}
-      />
+      {imageElement}
     </div>
+  ) : (
+    imageElement
   )
 }
 

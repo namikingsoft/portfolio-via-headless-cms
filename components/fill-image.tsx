afb1995b4d877da8d048bfa54103a57348115c46
Tsubasa Namiki <namikingsoft@gmail.com>
2022-11-13 20:01:34 +0900

    Fix fill image aspect ratio on related articles

--- components/fill-image.tsx
+++ components/fill-image.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import cn from 'classnames'
+import { useCallback, useState } from 'react'
 
 type Props = {
   src: string
@@ -24,13 +25,25 @@ const FillImage = ({
   blurSrc,
   className,
 }: Props) => {
-  const imageElement = (
-    <Image
-      src={src}
-      alt={alt}
-      width={width}
-      height={height}
-      sizes="
+  const [loaded, setLoaded] = useState(false)
+  const onLoad = useCallback(() => {
+    setLoaded(true)
+  }, [])
+
+  const baseClassName = cn(
+    className,
+    objectPosition === 'top' ? 'object-top' : 'object-center',
+    'object-cover min-w-full',
+  )
+
+  return (
+    <>
+      <Image
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        sizes="
   (max-width: 640px) 640px,
   (max-width: 800px) 800px,
   (max-width: 1024px) 1024px,
@@ -41,25 +54,23 @@ const FillImage = ({
   (max-width: 2560px) 2560px,
   3840px
 "
-      priority={priority}
-      className={cn(
-        !blurSrc ? className : undefined,
-        objectPosition === 'top' ? 'object-top' : 'object-center',
-        'object-cover min-w-full min-h-full',
+        priority={priority}
+        className={cn(baseClassName, {
+          hidden: !loaded && blurSrc,
+        })}
+        style={{ aspectRatio }}
+        onLoad={onLoad}
+      />
+      {blurSrc && !loaded && (
+        // eslint-disable-next-line @next/next/no-img-element -- only blur image
+        <img
+          src={blurSrc}
+          alt={alt}
+          className={baseClassName}
+          style={{ aspectRatio }}
+        />
       )}
-      style={{ aspectRatio }}
-    />
-  )
-
-  return blurSrc ? (
-    <div
-      className={cn(blurSrc ? className : undefined, 'bg-cover')}
-      style={{ backgroundImage: `url(${blurSrc})` }}
-    >
-      {imageElement}
-    </div>
-  ) : (
-    imageElement
+    </>
   )
 }
 

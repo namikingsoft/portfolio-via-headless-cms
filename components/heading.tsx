8f2dd6f84af619d9aecbb092d2b986f8d728b375
Tsubasa Namiki <namikingsoft@gmail.com>
2022-12-12 06:17:19 +0900

    Fix to pass className on heading

--- components/heading.tsx
+++ components/heading.tsx
@@ -10,14 +10,16 @@ type Props = {
 const Heading = ({ className, as: Component, children }: Props) => {
   return (
     <Component
-      className={cn({
-        'text-6xl lg:text-7xl font-bold tracking-tighter leading-tight mb-10':
-          Component === 'h1',
-        'mb-10 text-3xl lg:text-4xl xl:text-5xl font-bold tracking-tighter leading-tight':
-          Component === 'h2',
-        'text-2xl xl:text-3xl mb-4 leading-snug': Component === 'h3',
+      className={cn(
+        {
+          'text-6xl lg:text-7xl font-bold tracking-tighter leading-tight mb-10':
+            Component === 'h1',
+          'mb-10 text-3xl lg:text-4xl xl:text-5xl font-bold tracking-tighter leading-tight':
+            Component === 'h2',
+          'text-2xl xl:text-3xl mb-4 leading-snug': Component === 'h3',
+        },
         className,
-      })}
+      )}
     >
       {children}
     </Component>

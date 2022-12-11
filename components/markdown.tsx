effe952c6c4bed5e396a6d90b812a0b8240d9d73
Tsubasa Namiki <namikingsoft@gmail.com>
2022-12-11 12:25:53 +0900

    Forward ref to markdown component

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,4 +1,5 @@
 import cn from 'classnames'
+import { forwardRef } from 'react'
 import markdownStyles from './markdown.module.css'
 
 type Props = {
@@ -7,20 +8,25 @@ type Props = {
   children: string
 }
 
-const Markdown = ({ className, type, children }: Props) => {
-  return (
-    <div
-      className={cn(
-        markdownStyles.base,
-        {
-          [markdownStyles.detail]: type === 'detail',
-          [markdownStyles.lite]: type === 'lite',
-        },
-        className,
-      )}
-      dangerouslySetInnerHTML={{ __html: children }}
-    />
-  )
-}
+const Markdown = forwardRef<HTMLDivElement, Props>(
+  ({ className, type, children }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          markdownStyles.base,
+          {
+            [markdownStyles.detail]: type === 'detail',
+            [markdownStyles.lite]: type === 'lite',
+          },
+          className,
+        )}
+        dangerouslySetInnerHTML={{ __html: children }}
+      />
+    )
+  },
+)
+
+Markdown.displayName = 'Markdown'
 
 export default Markdown
